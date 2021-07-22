9e1389b2697611aac33f0a741dd4e49053e310fd
Talita Azevedo <talita.azevedo360@gmail.com>
2021-07-22 00:10:55 -0300

    Configuração de fonte externa e title Dinamico por pagina

---
+++ src/pages/_document.tsx
@@ -0,0 +1,25 @@
+// é carregado uma unica vez na minha aplicação
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+export default class MyDocument extends Document {
+    render() {
+        return (
+            <Html>
+                <Head>
+                    <link
+                        rel="preconnect"
+                        href="https://fonts.googleapis.com"
+                    />
+                    <link
+                        href="https://fonts.googleapis.com/css2?family=Roboto:wght@700;900&display=swap"
+                        rel="stylesheet"
+                    />
+                    <title>ig.news</title>
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
+}
