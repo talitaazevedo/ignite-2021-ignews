b82dc0f60818a846537d675944e2bfac5c1a30db
Talita Azevedo <talita.azevedo360@gmail.com>
2021-07-23 00:30:40 -0300

    Pagina Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,28 @@
 import Head from 'next/head';
+import Image from 'next/image';
+import styles from './home.module.scss';
+import avatarSVG from '../../public/images/avatar.svg';
+
 export default function Home() {
     return (
         <>
             <Head>
-                <title> Inicio | ig.news</title>
+                <title> Home | ig.news</title>
             </Head>
-            <h1>Hello World</h1>
+            <main className={styles.contentContainer}>
+                {' '}
+                <section className={styles.hero}>
+                    <span> 👏 Hey, welcome</span>
+                    <h1>
+                        News aboute the <span>React</span> world
+                    </h1>
+                    <p>
+                        Get access to all the articles on <br />
+                        <span>$ 9 month</span>
+                    </p>
+                </section>
+                <Image src={avatarSVG} alt="Girl coding" />
+            </main>
         </>
     );
 }
