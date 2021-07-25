7c9c647897d3cef62c861163b2490e686540032f
Talita Azevedo <talita.azevedo360@gmail.com>
2021-07-24 23:37:06 -0300

    salvando usuário no banco

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,8 @@
+import { query as q } from 'faunadb';
+
 import NextAuth from 'next-auth';
 import Providers from 'next-auth/providers';
-
+import { fauna } from '../../../services/fauna';
 export default NextAuth({
     providers: [
         Providers.GitHub({
@@ -10,4 +12,21 @@ export default NextAuth({
         }),
         // ...add more providers here
     ],
+    jwt: {
+        signingKey: process.env.JWT_NEXT_AUTH_SIGNING_KEY,
+    },
+    callbacks: {
+        async signIn(user, account, profile) {
+            const { email } = user;
+
+            try {
+                await fauna.query(
+                    q.Create(q.Collection('users'), { data: { email } }),
+                );
+                return true;
+            } catch (err) {
+                return false;
+            }
+        },
+    },
 });
