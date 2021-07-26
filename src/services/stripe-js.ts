621ebf44a7b6ca25afc8cfa37612ec2ffd17111d
Talita Azevedo <talita.azevedo360@gmail.com>
2021-07-26 20:39:38 -0300

    Redirecionando parao stripe

---
+++ src/services/stripe-js.ts
@@ -0,0 +1,10 @@
+// parte frontend do stripe
+
+import { loadStripe } from '@stripe/stripe-js';
+
+export async function getStripeJs() {
+    const stripeJs = await loadStripe(
+        process.env.NEXT_PUBLIC_STRIPE_API_PUBLIC_KEY,
+    );
+    return stripeJs;
+}
