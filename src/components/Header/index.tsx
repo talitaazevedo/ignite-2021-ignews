a092ad5c71f586205d0634d4fa075a571dc71786
Talita Azevedo <talita.azevedo360@gmail.com>
2021-07-31 23:43:15 -0300

    navegação do menu

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,14 +3,19 @@ import Image from 'next/image';
 import logoSvg from '../../../public/images/logo.svg';
 import React from 'react';
 import { SignInButton } from '../SignInButton';
+import Link from 'next/link';
 export function Header() {
     return (
         <header className={styles.headerContainer}>
             <div className={styles.headerContent}>
                 <Image src={logoSvg} alt=" ignews seu blog de tecnologia" />
                 <nav>
-                    <a className={styles.active}>Home</a>
-                    <a className={styles.active}>Posts</a>
+                    <Link href="/">
+                        <a className={styles.active}>Home</a>
+                    </Link>
+                    <Link href="/posts" prefetch>
+                        <a className={styles.active}>Posts</a>
+                    </Link>
                 </nav>
                 <SignInButton />
             </div>
