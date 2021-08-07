08c8c770a75f77028d7fdf2bd58a55644cc5c783
Talita Azevedo <talita.azevedo360@gmail.com>
2021-08-06 22:38:24 -0300

    validando assinatura ativa

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -17,6 +17,43 @@ export default NextAuth({
     ],
 
     callbacks: {
+        async session(session) {
+            try {
+                const { email } = session.user;
+
+                const userActiveSubscription = await fauna.query(
+                    q.Get(
+                        q.Intersection([
+                            q.Match(
+                                q.Index('subscription_by_user_ref'),
+                                q.Select(
+                                    'ref',
+                                    q.Get(
+                                        q.Match(
+                                            q.Index('user_by_email'),
+                                            q.Casefold(email),
+                                        ),
+                                    ),
+                                ),
+                            ),
+                            q.Match(
+                                q.Index('subscription_by_status'),
+                                'active',
+                            ),
+                        ]),
+                    ),
+                );
+                return {
+                    ...session,
+                    activeSubscription: userActiveSubscription,
+                };
+            } catch {
+                return {
+                    ...session,
+                    activeSubscription: null,
+                };
+            }
+        },
         async signIn(user, account, profile) {
             const { email } = user;
 
--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -41,28 +41,12 @@ export const getServerSideProps: GetServerSideProps = async ({
     const session = await getSession({ req });
     const { slug } = params;
 
-    //  if(!session) {}
-
-    // Verificar assinatura ativa
-
-    const prismic = getPrismicClient(req);
-    const response = await prismic.getByUID('post', String(slug), {});
-
-    const post = {
-        slug,
-        title: RichText.asText(response.data.title),
-        content: RichText.asHtml(response.data.content),
-        updatedAt: new Date(response.last_publication_date).toLocaleDateString(
-            'pt-BR',
-            {
-                day: '2-digit',
-                month: 'long',
-                year: 'numeric',
+    if (!session.activeSubscription) {
+        return {
+            redirect: {
+                destination: '/',
+                permanent: false,
             },
-        ),
-    };
-
-    return {
-        props: { post },
-    };
+        };
+    }
 };
